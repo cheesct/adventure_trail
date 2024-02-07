b444dd2abf003d38386a3a815d87ee713694af74
cheesct <chesscheese_chees@yahoo.com.vn>
2024-02-07 11:24:03 +0700

    no message

--- src/Scene_Level1d.ts
+++ src/Scene_Level1d.ts
@@ -20,7 +20,7 @@ export default class Scene_Level1d extends LevelBase
 	    this.load.image('grass', 'assets/level1/grass.png')
 	    this.load.image('mountains1', 'assets/level1/mountains1.png')
 	    this.load.image('mountains2', 'assets/level1/mountains2.png')
-	    this.load.tilemapTiledJSON('level1c', 'assets/level1/map1c.json')
+	    this.load.tilemapTiledJSON('level1d', 'assets/level1/map1d.json')
   	}
 
   	create() 
@@ -30,16 +30,16 @@ export default class Scene_Level1d extends LevelBase
 		this.ParallaxStatic.add(new ParallaxStaticTileSprite(this, 0, 82, 320, 32, "mountains2", 0.125))
 		this.ParallaxStatic.add(new ParallaxStaticTileSprite(this, 0, 88, 320, 32, "mountains1", 0.25))
 		this.ParallaxStatic.add(new ParallaxStaticTileSprite(this, 0, 112, 320, 64, "grass", 0.5))
-		const map = this.initialize_map('level1c', 'level1_walls2', 'level1_props', 'level1_backs')
+		const map = this.initialize_map('level1d', 'level1_walls2', 'level1_props', 'level1_backs')
 		const emitter = this.add.particles(0, 0, 'flares', {
             frame: 'white',
             blendMode: 'ADD',
             quantity: 1,
             lifespan: { min: 5000, max: 10000 } ,
-            frequency: 100,
-            scale: { start: 0.4, end: 0, random: true },
+            frequency: 50,
+            scale: { start: 0.3, end: 0, random: true },
             speed: { min: 0, max: 5 },
-            alpha: { values: [ 1, 0, 1 ], interpolation: 'linear' },
+            alpha: { values: [ 0, 0.5, 0 ], interpolation: 'linear' },
             angle: { min: 0, max: 360 },
             emitZone: { source: new Phaser.Geom.Rectangle(0, 0, map.widthInPixels, map.heightInPixels), type: 'random' }
         })
